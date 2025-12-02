096178589f35299122e309d60be640a7a5fe96de
farrko <renanricci01@gmail.com>
2025-12-02 14:17:33 -0300

    Feita lógica de pontos e coordenadas polares

---
+++ src/shapes/point.h
@@ -0,0 +1,31 @@
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct point_t point_t;
+typedef struct polar_coords_t polar_coords_t;
+
+point_t *point_init(double x, double y);
+polar_coords_t *polar_init(double angle, double distance, point_t *origin);
+
+void point_destroy(void *point);
+void polar_destroy(void *polar);
+
+double point_get_x(point_t *point);
+double point_get_y(point_t *point);
+void point_set_x(point_t *point, double x);
+void point_set_y(point_t *point, double y);
+
+double polar_get_angle(polar_coords_t *polar);
+double polar_get_distance(polar_coords_t *polar);
+point_t *polar_get_origin(polar_coords_t *polar);
+
+void polar_set_angle(polar_coords_t *polar, double angle);
+void polar_set_distance(polar_coords_t *polar, double distance);
+void polar_set_origin(polar_coords_t *polar, point_t *origin);
+
+double polar_get_absolute_x(polar_coords_t *polar);
+double polar_get_absolute_y(polar_coords_t *polar);
+
+polar_coords_t *polar_from_cartesian(point_t *origin, point_t *final);
+
+#endif
